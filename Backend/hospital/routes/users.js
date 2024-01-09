fdce6c68442969276c8439a175912d68f5a311e5
ryuga123677 <kishanss311@gmail.com>
2024-01-09 20:09:13 +0530

    ..

--- Backend/hospital/routes/users.js
+++ Backend/hospital/routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const plm= require("passport-local-mongoose");
 require('dotenv').config();
-mongoose.connect(process.env.DATABASE);//mongodb://127.0.0.1:27017/Hospital-Database
+mongoose.connect(process.env.MONGO_URI);//mongodb://127.0.0.1:27017/Hospital-Database
 
 const userSchema = new mongoose.Schema({
   username: {
